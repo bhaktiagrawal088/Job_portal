1b23847ceaa1a65fe7778398d0d4e772c62ecfd8
unknown <bhaktiagrawal397@gmail.com>
2024-09-28 03:26:09 +0530

    Jon Controller and API Routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connetDB from "./utilis/db.js";
 import userRoute  from "./routes/user.routes.js"
 import companyRoute from "./routes/company.routes.js"
+import jobRoute from "./routes/job.routes.js"
 
 dotenv.config({})
 const app = express();
@@ -31,6 +32,7 @@ const PORT = process.env.PORT || 3000;
 //api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
 
 app.listen(PORT, () => {
     connetDB();
--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -21,9 +21,17 @@ const jobSchema = new mongoose.Schema({
         type : String,
         required : true
     },
+    experienceLevel : {
+        type : Number,
+        required : true
+    },
     jobType : {
         type : String,
         required : true 
+    },
+    position : {
+        type : Number,
+        required : true
     },
     company : {
         type : mongoose.Schema.Types.ObjectId, //  relation between company and job
